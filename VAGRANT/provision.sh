b598e49903e74ec9799b6b3abfa56f8dfc8e4855
Mark Cunningham <mark@Marks-PC>
2023-12-09 14:40:37 -0600

    delete unnecessary files

--- VAGRANT/provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get upgrade -y
-
-apt-get install -y build-essential
-
-# I don't want to upset Dr. Veras too much so I am also installing emacs, but I put neovim first for a reason ;)
